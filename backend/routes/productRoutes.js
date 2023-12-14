b2d13cd48081345fb6ac4df6ce5f6552f47adb55
Mohit Khamari <mohitkhamarim24@gmail.com>
2023-12-14 12:45:40 +0530

    Created backend ,connected to mongodp fetched data from database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const router =express.Router();
+import products from '../data/products.js';
+
+
+router.get('/', async(req,res) =>{
+    res.json(products);
+});
+router.get('/:id' ,(req, res) => {
+    const product = products.find((p) =>p._id===req.params.id);
+    res.json(product);
+});
+
+
+
+export default router;
