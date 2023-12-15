fdc34d91e4c29ca9f5bba4dc7fce201fbbb8d36e
Mohit Khamari <mohitkhamarim24@gmail.com>
2023-12-15 13:34:52 +0530

    starting with redux

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,7 @@
 import express from 'express';
 const router =express.Router();
-import products from '../data/products.js';
+import { getProducts,getProductById } from '../controllers/productController.js';
 
-
-router.get('/', async(req,res) =>{
-    res.json(products);
-});
-router.get('/:id' ,(req, res) => {
-    const product = products.find((p) =>p._id===req.params.id);
-    res.json(product);
-});
-
-
-
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 export default router;
