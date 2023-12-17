10295c929084fcb2a24d654feb942e8b64346b5a
Mohit Khamari <mohitkhamarim24@gmail.com>
2023-12-17 17:11:01 +0530

    Adding shopping cart

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,37 @@
+import {Link, useNavigate} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {Row,Col, ListGroup,Image,Form,Button,Card} from 'react-bootstrap';
+import {faTrash} from 'react-icons/fa';
+import Message from '../components/Message';
+
+const CartScreen = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const cart = useSelector((state)=> state.cart);
+    const {cartItems} =cart; 
+  return(
+    <Row>
+    <Col md = {8}>
+
+        <h1 style={{marginBottom :'20px'}}>Shopping Cart</h1>
+        {cartItems.length ===0 ? (
+            <Message>
+                Your Cart is empty<Link to = '/'>Go Back</Link>
+            </Message>
+        ):(
+
+            <ListGroup varient = 'flush'>
+
+            Items
+
+            </ListGroup>
+        )}
+    </Col>
+
+    </Row>
+    
+  )
+}
+
+export default CartScreen;
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,15 +1,30 @@
-import {useParams} from 'react-router-dom';
+import { useState } from 'react';
+import {useParams,useNavigate} from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import {Row,Col,ListGroup,Card,Button} from 'react-bootstrap';
+import {Form,Row,Col,ListGroup,Card,Button} from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+import {addToCart} from '../slices/cartSlices';
 const ProductScreen = () => {
 
     const {id:productId} =useParams();
+    
+    const dispatch = useDispatch();
+    
+    const navigate = useNavigate();
+
+    const [qty,setQty] = useState(1)
    
-    const{data: product,isLoading,error}=useGetProductDetailsQuery(productId)
+  
+    const{data: product,isLoading,error}=useGetProductDetailsQuery(productId);
+
+    const addToCartHandler = ()=>{
+        dispatch(addToCart({...product,qty}))
+        navigate('/cart');
+    }
 
   return (
     <>
@@ -55,8 +70,26 @@ const ProductScreen = () => {
                      <Col><strong>${product.countInStock >0? 'In Stock':'Out Of Stock'}</strong></Col>
                      </Row>
                  </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                        <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                        <Form.Control as = 'select' value ={qty} onChange={(e) => setQty(Number(e.target.value))}>
+                            {[...Array(product.countInStock).keys()].map((x) => (
+                                <option key = {x+1} value= {x+1}>
+                                {x+1}
+                                </option>
+                            ))}
+                        </Form.Control>
+                        </Col>
+                        </Row>
+                    </ListGroup.Item>
+                )}
+
                  <ListGroup.Item>
-                     <Button className='btn-block' type='button' disabled={product.countInStock===0}>Add to Cart</Button>
+                     <Button className='btn-block' type='button' disabled={product.countInStock===0} onClick={addToCartHandler}>Add to Cart</Button>
                  </ListGroup.Item>
              </ListGroup>
          </Card>
